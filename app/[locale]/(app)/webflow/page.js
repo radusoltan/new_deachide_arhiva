f3c1bf42a3c860def71a4d91561399e43117cfa7
Radu Soltan <radusoltan@gmail.com>
2024-12-10 13:32:16 +0200

    added icons and seo settings 1;

--- app/[locale]/(app)/webflow/page.js
+++ app/[locale]/(app)/webflow/page.js
@@ -1,14 +1,14 @@
-const getWebflowItems = async ()=>{
-
-  const response = await fetch("http://localhost:3000/api/content/articles/webflow")
-
-  return await response.json()
-
-}
+// const getWebflowItems = async ()=>{
+//
+//   const response = await fetch("http://localhost:3000/api/content/articles/webflow")
+//
+//   return await response.json()
+//
+// }
 const WebFlowPage = async ()=>{
-  const {items, pagination} = await getWebflowItems()
+  // const {items, pagination} = await getWebflowItems()
 
-  console.log(pagination)
+  // console.log(pagination)
 
   return <>WebFlowPage</>
 }
